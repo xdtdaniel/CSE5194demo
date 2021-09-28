1a11329d5c3c57c46463ce1dfe971e6a05ae4d76
Dantong Xue <xdtdaniel@gmail.com>
2021-09-28 18:16:42 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -14,12 +14,12 @@ df_to_plot = df_time[df_time['asset_id'] == 1]
 fig = go.Figure(data=[go.Candlestick(x=df_to_plot['time'],
                 open=df_to_plot['open'], high=df_to_plot['high'],
                 low=df_to_plot['low'], close=df_to_plot['close'])
-                     ])
+                     ], title='Bitcoin Price')
 
-fig.update_layout(xaxis_rangeslider_visible=False)
+fig.update_layout(xaxis_rangeslider_visible=True)
 st.plotly_chart(fig)
 
 # st.plotly_chart(px.scatter(df_time[df_time['asset_id'] == 1], x='time', y='open'))
 # Plot Volatility
-st.plotly_chart(px.scatter(df_time[df_time['asset_id'] == 1], x='time', y='volatility'))
+st.plotly_chart(px.scatter(df_time[df_time['asset_id'] == 1], x='time', y='volatility', , title='Bitcoin Volatility'))
 st.write(prettyprint_news.prettyprint(extract_news.searchCryptoNews('2021-09-20', '2021-09-21'), 10), unsafe_allow_html=True)
