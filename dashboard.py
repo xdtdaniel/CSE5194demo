a8e7368873ee268af0b408f62feb635e52ae25c0
Dantong Xue <xdtdaniel@gmail.com>
2021-10-05 18:46:25 -0400

    Update formatting

--- dashboard.py
+++ dashboard.py
@@ -6,10 +6,20 @@ import prettyprint_news
 import plotly.graph_objects as go
 from transformers import pipeline
 
+label_map = {'LABEL_0': 'Negative', 'LABEL_1': 'Neutral', 'LABEL_2': 'Positive'}
+
+classifier = pipeline('sentiment-analysis', model="cardiffnlp/twitter-roberta-base-sentiment")
+result = classifier('We are very happy to show you the 🤗 Transformers library.')
+score = result[0]
+label = label_map[result[0]['label']]
+
+
 
 st.set_page_config(layout="wide")
 st.title('Crypto Dashboard')
 df_coin, df_time = market_data.obtain_market_data()
+
+st.write(f'{label}, {score}')
 
 col1, col2 = st.columns(2)
 
@@ -26,8 +36,4 @@ col1.plotly_chart(fig)
 # st.plotly_chart(px.scatter(df_time[df_time['asset_id'] == 1], x='time', y='open'))
 # Plot Volatility
 col2.plotly_chart(px.scatter(df_time[df_time['asset_id'] == 1], x='time', y='volatility', title='Bitcoin Volatility'))
-classifier = pipeline('sentiment-analysis', model="cardiffnlp/twitter-roberta-base-sentiment")
-result = classifier('We are very happy to show you the 🤗 Transformers library.')
-print(result)
-st.write(result)
 st.write(prettyprint_news.prettyprint(extract_news.searchCryptoNews('2021-09-20', '2021-09-21'), 10), unsafe_allow_html=True)
