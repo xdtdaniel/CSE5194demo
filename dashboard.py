46154fe582b99232bdcb20ec49c778b4df1a33d6
Dantong Xue <xdtdaniel@gmail.com>
2021-11-14 21:57:47 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -63,8 +63,8 @@ col1.plotly_chart(fig)
 # st.plotly_chart(px.scatter(df_time[df_time['asset_id'] == 1], x='time', y='open'))
 # Plot Volatility
 col2.plotly_chart(px.scatter(df_time[df_time['asset_symbol'] == curr_coin_symbol], x='time', y='volatility', title=f'{curr_coin_symbol} Volatility'))
-news_md, news_senti_labels = prettyprint_news_watson.prettyprint(news_query.get_news(curr_coin_symbol), 50)
+news_md, news_senti_labels = prettyprint_news_watson.prettyprint(news_query.get_news(curr_coin_symbol), 10)
 st.write(news_md, unsafe_allow_html=True)
-col3.plotly_chart(px.histogram(news_senti_labels, title='Top 50 News Sentiment'))
+# col3.plotly_chart(px.histogram(news_senti_labels, title='Top 50 News Sentiment'))
 
 col4.plotly_chart(fig3)
